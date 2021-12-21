ce67e5ae3acf3768d7122f9d1f8eebd0f8ea00a4
yjkwon07 <dudwns9073@nate.com>
2021-12-22 08:45:12 +0900

    [config]: globalStyle color 적용

--- src/assets/styles/GlobalStyles.tsx
+++ src/assets/styles/GlobalStyles.tsx
@@ -1,5 +1,49 @@
-import { GlobalStyles as BaseStyles } from 'twin.macro';
+import { Global, css } from '@emotion/react';
+import { GlobalStyles as BaseStyles, theme } from 'twin.macro';
 
-const GlobalStyles = () => <BaseStyles />;
+const customStyles = css([
+  {
+    body: {
+      fontFamily:
+        'Helvetica Neue, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, sans-serif',
+      backgroundColor: theme`colors.primary`,
+      color: theme`textColor.primary`,
+      fontSize: 11,
+      lineHeight: 1.4,
+      fontWeight: 500,
+      overflowX: 'hidden',
+      marginBottom: 72,
+    },
+  },
+  {
+    ...css`
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        margin: 0;
+        font-weight: 400;
+        line-height: 1;
+      }
+
+      h1 {
+        font-size: 30px;
+      }
+
+      h2 {
+        font-size: 24px;
+      }
+    `,
+  },
+]);
+
+const GlobalStyles = () => (
+  <>
+    <BaseStyles />
+    <Global styles={customStyles} />
+  </>
+);
 
 export default GlobalStyles;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#bdcdd6',
+        yellow: {
+          200: '#ffe6cf',
+          300: '#fedd15',
+          400: '#ffc000',
+        },
+      },
+      textColor: {
+        primary: '#ffe6cf',
+        secondary: '#485861',
+      },
+    },
   },
   variants: {
     extend: {},
