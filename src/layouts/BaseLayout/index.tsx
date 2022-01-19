f172affacb8125d328452fb9a7797ad5c758a994
yjkwon07 <dudwns9073@nate.com>
2022-01-19 22:53:49 +0900

    [layout]: menuDepth1 => menuDepth1List  변수 명칭 변경

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -11,7 +11,7 @@ const BaseLayout: FC = ({ children }) => {
   const location = useLocation();
 
   const menuList = menuFlattenList;
-  const menuDepth1 = useMemo(() => menuList.filter((menu) => menu.show && menu.depth === 1), [menuList]);
+  const menuDepth1List = useMemo(() => menuList.filter((menu) => menu.show && menu.depth === 1), [menuList]);
   const activeMenuList = useMemo(() => getActiveMenuList(menuList, location.pathname), [location.pathname, menuList]);
   const activeId = useMemo(
     () => activeMenuList.find((activeMenu) => activeMenu.depth === 1)?.menuId || '',
@@ -21,7 +21,7 @@ const BaseLayout: FC = ({ children }) => {
   return (
     <>
       <div tw="mb-72">{children}</div>
-      <Navbar menuList={menuDepth1} activeId={activeId} />
+      <Navbar menuList={menuDepth1List} activeId={activeId} />
     </>
   );
 };
