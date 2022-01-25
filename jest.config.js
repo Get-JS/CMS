3866b9bc79f31fe0fcc4470d2ce6edeac978da87
yjkwon07 <dudwns9073@nate.com>
2022-01-25 11:36:43 +0900

    [config]: jest config 수정

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  testRegex: '.test.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.+)$': '<rootDir>/src/$1',
+  },
+  modulePaths: ['<rootDir>'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
 };
