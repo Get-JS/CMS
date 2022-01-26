827ed3f675bd3c46d9f982e0559d0aa6f1b0f42d
yjkwon07 <dudwns9073@nate.com>
2022-01-26 18:19:04 +0900

    [config]: svg-jest 추가

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
+  modulePaths: ['<rootDir>'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
   testRegex: '.test.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.svg$': 'svg-jest',
   },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
   moduleNameMapper: {
+    '@assets/(.+)$': '<rootDir>/src/assets/$1',
+    '@styles/(.+)$': '<rootDir>/src/assets/styles/$1',
+    '@images/(.+)$': '<rootDir>/src/assets/images/$1',
     '@/(.+)$': '<rootDir>/src/$1',
   },
-  modulePaths: ['<rootDir>'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
 };
