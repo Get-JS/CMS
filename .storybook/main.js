10cb6db1ea956d0ea5dbc403b2b40f7424eeeded
yjkwon07 <dudwns9073@nate.com>
2022-02-24 08:40:00 +0900

    [config]: storybook emotion, twin 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+
 module.exports = {
-  core: {
-    builder: 'webpack5',
+  stories: ['../src/components/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+  babel: async (options) => {
+    options.plugins.unshift('babel-plugin-twin');
+    options.presets.push('@emotion/babel-preset-css-prop');
+    return options;
   },
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      '@emotion/core': getPackageDir('@emotion/react'),
+      '@emotion/styled': getPackageDir('@emotion/styled'),
+    };
+    return config;
+  },
 };
+
+// Fix for package resolution
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+    }
+    currDir = dir;
+  }
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import { GlobalStyles } from 'twin.macro';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cache = createCache({ prepend: true, key: 'twin' });
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <CacheProvider value={cache}>
+      <GlobalStyles />
+      <Story />
+    </CacheProvider>
+  ),
+];
