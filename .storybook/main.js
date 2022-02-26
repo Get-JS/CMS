2932894803d38a8c8343a3c8fa77c9d3ce75983e
yjkwon07 <dudwns9073@nate.com>
2022-02-26 14:43:43 +0900

    [comfig]: storybook main webpack babel sync - babel   - preset   - plugin - webpack   - alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,40 @@
 const path = require('path');
 const fs = require('fs');
 
+const customWebpackConfig = require('../craco.config.js');
+
 module.exports = {
   stories: ['../src/components/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
   babel: async (options) => {
-    options.plugins.unshift('babel-plugin-twin');
-    options.presets.push('@emotion/babel-preset-css-prop');
+    const { babel } = customWebpackConfig;
+    const pluginsPackageNameList = babelOptionFlatName(babel.plugins);
+    const presetsPackageNameList = babelOptionFlatName(babel.presets);
+
+    options.plugins = options.plugins
+      .filter((plugin) => isDuplicatePackage(plugin, pluginsPackageNameList))
+      .concat(toPackageAbsolute(babel.plugins));
+
+    options.presets = options.presets
+      .filter((preset) => isDuplicatePackage(preset, presetsPackageNameList))
+      .concat(toPackageAbsolute(babel.presets));
+
     return options;
   },
   webpackFinal: async (config) => {
     config.resolve.alias = {
-      '@emotion/core': getPackageDir('@emotion/react'),
-      '@emotion/styled': getPackageDir('@emotion/styled'),
+      '@emotion/core': packageAbsoluteDir('@emotion/react'),
+      '@emotion/styled': packageAbsoluteDir('@emotion/styled'),
+      '@': resolve('../src'),
+      '@assets': resolve('../src/assets'),
+      '@styles': resolve('../src/assets/styles'),
+      '@images': resolve('../src/assets/images'),
     };
     return config;
   },
 };
 
-// Fix for package resolution
-function getPackageDir(filepath) {
+function packageAbsoluteDir(filepath) {
   let currDir = path.dirname(require.resolve(filepath));
   while (true) {
     if (fs.existsSync(path.join(currDir, 'package.json'))) {
@@ -31,4 +46,35 @@ function getPackageDir(filepath) {
     }
     currDir = dir;
   }
+}
+
+function babelOptionFlatName(optionPackageList) {
+  return optionPackageList.map((optionPackage) => {
+    if (Array.isArray(optionPackage)) {
+      return packageAbsoluteDir(optionPackage[0]);
+    }
+    return packageAbsoluteDir(optionPackage);
+  });
+}
+
+function isDuplicatePackage(option, currentPackageList) {
+  if (Array.isArray(option)) {
+    return (
+      typeof option[0] === 'string' && !currentPackageList.find((currentPackage) => option[0].includes(currentPackage))
+    );
+  }
+  return !currentPackageList.find((currentPackage) => option.includes(currentPackage));
+}
+
+function toPackageAbsolute(packageList) {
+  return packageList.map((package) => {
+    if (Array.isArray(package) && typeof package[0] === 'string') {
+      return [packageAbsoluteDir(package[0]), ...package.slice(1)];
+    }
+    return packageAbsoluteDir(package);
+  });
+}
+
+function resolve(arg) {
+  return path.resolve(__dirname, arg);
 }
