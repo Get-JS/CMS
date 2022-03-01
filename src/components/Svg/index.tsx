ea3be956598a8a706b23c535899cef466cddd215
yjkwon07 <dudwns9073@nate.com>
2022-03-01 17:10:19 +0900

    [component]: Svg Icon 추가

---
+++ src/components/Svg/index.tsx
@@ -0,0 +1,5 @@
+export { default as AutoRenewIcon } from './Icons/AutoRenew';
+export { default as SearchIcon } from './Icons/Search';
+
+export { default as Svg } from './Svg';
+export type { SvgProps } from './@types';
